7df9d0d2eec1344f457734bc078b070d659fb305
ymgwon-nportverse <ymgwon@nportverse.com>
2023-11-03 18:29:53 +0900

    🎉 init: initialize project

---
+++ android/app/src/main/kotlin/com/nportverse/poc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nportverse.poc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/test/kotlin/com/nportverse/poc/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.nportverse.poc
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun shouldBeTrue() {
+        val result = 1 + 1
+        assertEquals(2, result)
+    }
+}
